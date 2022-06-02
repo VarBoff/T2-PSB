7a1085769e2577d80b0cbf75bd35f148cf86caf0
Eduardo d'Alençon <84420706+eppdalencon@users.noreply.github.com>
2022-06-02 20:12:16 -0300

    Adição de nome do programa e dos autores

--- main.c
+++ main.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+#define PROGRAM_NAME "ig"
+
+#define AUTHORS proper_name ("Eduardo d'Alençon, Pedro Machado, Vinícius Boff")
+
 int main(){
     printf("Página principal");
     return 0;
