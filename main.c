fec9b4ce7c6fd31297cf216aed8e1aeeaa555b39
VarBoff <vinirechboff@gmail.com>
2022-06-08 14:14:41 -0300

    Mudanças na main para suportar a criação do arquivo readfile.c. Mudança adicionada para guardar as palavras lidas em heap

--- main.c
+++ main.c
@@ -1,73 +1,33 @@
 /* gerarIndicesRemissivos
 
-   Copyright (C) 1990-2022 Free Software Foundation, Inc.
+Copyright (C) 1990-2022 Free Software Foundation, Inc.
 
-   This program is free software: you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation, either version 3 of the License, or
-   (at your option) any later version.
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
-   You should have received a copy of the GNU General Public License
-   along with this program.  If not, see <https://www.gnu.org/licenses/>.  */
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.  */
 
-#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "readFile.h"
 
 #define PROGRAM_NAME "ig"
 #define AUTHORS proper_name("Eduardo d'Alençon, Pedro Machado, Vinícius Boff")
 
-/* The value to return to the calling program.  */
-static int exit_status;
-
-int my_getline (char **restrict line, size_t *restrict len, FILE *restrict fp){
-      if( line == NULL || len== NULL){
-            fputs("Erro", stderr);
-            return -1;
-      }
-      if( fp == NULL){
-            fputs("Erro", stderr);
-            return -1;
-      }
-
-      char chunk[128];
-
-      if(*line == NULL){
-            *len = sizeof(chunk);
-            if((*line = malloc(*len)) == NULL){
-                  perror("Erro de memória");
-                  return -1;
-            }
-      }
-      (*line)[0] = '\0';
-
-      while(fgets(chunk, sizeof(chunk), fp) != NULL){
-            if(*len - strlen(*line) < sizeof(chunk)){
-                  *len *= 2;
-                  if((*line = realloc(*line,*len )) == NULL){
-                        perror("Erro de realocação");
-                        free(line);
-                        return -1;
-                  }
-            }
-
-            strcat(*line,chunk);
-
-            if((*line)[strlen(*line) - 1] == '\n'){
-                  return strlen(*line);
-            }
-      }
-      return -1;
-}
-
 int main()
 {
+      size_t totalLines = 0;
+      size_t totalWords = 0;
+      size_t actualLine;
 
       FILE *fp = fopen("poema.txt", "r");
       if (fp == NULL)
@@ -76,17 +36,14 @@ int main()
             return EXIT_FAILURE;
       }
 
-      // Read lines from file, allocate memory and build an array of lines
-      // -----------------------------------------------------------------
-      char *lineBuf = NULL;
-      size_t n = 0;
-      size_t nLines = 0;
-      size_t numLines = 0;
-      size_t nWords = 0;
-      ssize_t lineLength = 0;
-      size_t sizeIncrement = 10;
-      char **lines = malloc(sizeIncrement * sizeof(char **));
-      size_t i = 0;
+      puts("Arquivo aberto!");
+
+      // chama função de leitura
+
+      char **lines = readFileToArray(fp, &totalLines);
+
+      puts("Arquivo lido e alocado em heap");
+      // continua
 
       struct word
       {
@@ -95,99 +52,72 @@ int main()
             int contagem;
       };
 
-      struct word words[500];
+      struct word *words = malloc(200 * sizeof(struct word));
 
-      while ((lineLength = my_getline(&lineBuf, &n, fp)) != -1)
-      {
-            // Memory reallocation is expensive - don't reallocate on every iteration.
-            if (i >= sizeIncrement)
-            {
-                  sizeIncrement += sizeIncrement;
-
-                  // Don't just overwrite with realloc - the original
-                  // pointer may be lost if realloc fails.
-                  char **tmp = realloc(lines, sizeIncrement * sizeof(char **));
-                  if (!tmp)
-                  {
-                        perror("realloc");
-                        return EXIT_FAILURE;
-                  }
-                  lines = tmp;
-            }
-            // Remove \n from the line.
-            lineBuf[strcspn(lineBuf, "\n")] = 0;
-
-            // Allocate space on the heap for the line.
-            *(lines + i) = malloc((lineLength + 1) * sizeof(char));
-
-            // Copy the getline buffer into the new string.
-            strcpy(*(lines + i), lineBuf);
-
-            i++;
-
-            // Keep track of the number of lines read for later use.
-            nLines = i;
-      }
+      puts("Vetor de struct words criado");
 
       // Do something with the array of strings.
-      for (size_t k = 0; k < nLines; k++)
+      printf("totallines=%llu\n", totalLines);
+
+      for (actualLine = 1; actualLine < totalLines + 1; actualLine++)
       {
-            
-            numLines++;
-            char delim[] = " \n,:;.\"";
-            char *ptr = strtok(*(lines + k), delim);
+            printf("Actual line=%llu\n", actualLine);
+            char delim[] = " ,:;.\"";
+            char *ptr = strtok(*(lines + actualLine - 1), delim);
+
             while (ptr != NULL)
             {
                   // Aqui dentro, ptr é cada palavra lida
-                  printf("%s|", ptr);
+                  printf("Found=%s\n", ptr);
 
                   int found = 0;
+                  struct word *wordsPtr = words;
 
-                  for (int i = 0; i < 500; i++)
+                  for (size_t i = 0; i < totalWords; i++, wordsPtr++)
                   {
-                        if (!strcmp(words[i].palavra, ptr) && 
-                        words[i].linha[words[i].contagem - 1] != numLines)
+                        if (!strcmp(wordsPtr->palavra, ptr) &&
+                            wordsPtr->linha[wordsPtr->contagem - 1] != actualLine)
                         {
-                              words[i].linha[words[i].contagem] = numLines;
-                              words[i].contagem++;
+                              printf("Found duplicate in index=%llu\n", i);
+                              wordsPtr->linha[wordsPtr->contagem] = actualLine;
+                              wordsPtr->contagem++;
                               found = 1;
                         }
                   }
 
                   if (!found)
                   {
-                        strcpy(words[nWords].palavra, ptr);
-                        words[nWords].linha[words[nWords].contagem] = numLines;
-                        words[nWords].contagem++;
-                        nWords++;
+                        printf("New word! Adding in=%llu\n", totalWords);
+                        strcpy(&*(words + totalWords)->palavra, ptr);
+                        (words + totalWords)->linha[(words + totalWords)->contagem] = actualLine;
+                        (words + totalWords)->contagem++;
+                        totalWords++;
                   }
-                  
+
                   ptr = strtok(NULL, delim);
             }
             printf("\n");
       }
 
-      printf("\nnLines: %lu", nLines);
-      printf("\nnWords: %lu", nWords);
+      printf("\nnLines: %llu", totalLines);
+      printf("\nnWords: %llu", totalWords);
 
-      for (int i = 0; i < nWords; i++)
+      struct word *wordsPtr = words;
+      for (int i = 0; i < totalWords; i++, wordsPtr++)
       {
-            printf("\nPalavra: %s", words[i].palavra);
+            printf("\nPalavra: %s", wordsPtr->palavra);
 
-            for (int j = 0; j < words[i].contagem; j++)
+            for (int j = 0; j < wordsPtr->contagem; j++)
             {
-                  printf("\n   Linha %u", words[i].linha[j]);
+                  printf("\n\tLinha %llu", wordsPtr->linha[j]);
             }
       }
 
-      // Free the buffer utilised by `getline()`.
-      free(lineBuf);
-
       // Free the array of strings.
-      for (size_t i = 0; i < nLines; i++)
+      for (size_t i = 0; i < totalLines; i++)
             free(*(lines + i));
       free(lines);
 
-      fclose(fp);
+      free(words);
       return 0;
 }
