c7fddfb7f7dee3df2710575ad45ea929ad91a0b5
Pedro Semensato <semensatopedro@gmail.com>
2022-06-09 17:44:14 -0300

    Ajuste de Copy

--- main.c
+++ main.c
@@ -1,19 +1,15 @@
 /* gerarIndicesRemissivos
 
-Copyright (C) 1990-2022 Free Software Foundation, Inc.
+This program was developed for the Basic 
+Software course, taught by Professor 
+Marco Aurélio Mangan.
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+It serves as an academic piece 
+for evaluation and therefore 
+has no warranty. 
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>.  */
+Authors: Eduardo D'alençon, Pedro Semensato, Vinicius Boff
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
